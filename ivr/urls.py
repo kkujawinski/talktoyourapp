dbbe506ff2c47678a820f27f5460cb69319aa1fe
Kamil Kujawinski <kamil@kujawinski.net>
2015-10-18 12:03:16 +0200

    hotfix

--- ivr/urls.py
+++ ivr/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = patterns(
     '',
-    url(r'^welcome/(?P<participant_id>[\d])/$', views.welcome, name='welcome'),
-    url(r'^question/(?P<participant_id>[\d])/$', views.question, name='question'),
-    url(r'^answer/(?P<participant_id>[\d])/(?P<question_id>[\d])/$', views.answer, name='answer'),
+    url(r'^welcome/(?P<participant_id>[\d]+)/$', views.welcome, name='welcome'),
+    url(r'^question/(?P<participant_id>[\d]+)/$', views.question, name='question'),
+    url(r'^answer/(?P<participant_id>[\d]+)/(?P<question_id>[\d])/$', views.answer, name='answer'),
     url(r'^call_status/$', views.call_status, name='call_status'),
-    url(r'^init/(?P<participant_id>[\d])/$', views.inititate, name='init'),
+    url(r'^init/(?P<participant_id>[\d]+)/$', views.inititate, name='init'),
 )
